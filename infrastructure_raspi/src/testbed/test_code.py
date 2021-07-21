396e908991c4c026b61768c14eeed83d8d31e3ca
JCampbell9 <campbjos@oregonstate.edu>
2021-07-21 23:48:07 +0000

    button Test

--- infrastructure_raspi/src/testbed/test_code.py
+++ infrastructure_raspi/src/testbed/test_code.py
@@ -5,11 +5,12 @@ import RPi.GPIO as gpio
 
 
 if __name__ == '__main__':
-
-    gpio.setup(button_pin, gpio.IN) #, pull_up_down = gpio.PUD_UP)
+    button_pin = 2
+    gpio.setmode(gpio.BCM)
+    gpio.setup(button_pin, gpio.IN, pull_up_down = gpio.PUD_UP)
     
     while True:
 
-        if gpio.input(button_pin) == HIGH:
+        if gpio.input(button_pin) == False:
             print("button was pressed!!!!!!!!!!!")
         
